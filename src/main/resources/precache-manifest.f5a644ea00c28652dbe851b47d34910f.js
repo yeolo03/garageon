6c8fada7ae6468a00c8a67a8dc1e2fee24bb5f5f
yeolo03 <63214193+yeolo03@users.noreply.github.com>
2020-04-27 09:33:11 +0900

    Add files via upload

---
+++ src/main/resources/precache-manifest.f5a644ea00c28652dbe851b47d34910f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf2549ccd4d0825aa1f3ba761c920e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd57bfd100be442d8be6",
+    "url": "/static/js/2.cf6acb9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf6acb9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6934868e6ad002cac589",
+    "url": "/static/js/main.45ee38df.chunk.js"
+  },
+  {
+    "revision": "ff16104b09672a9ad72d",
+    "url": "/static/js/runtime-main.0ca84a5d.js"
+  }
+]);

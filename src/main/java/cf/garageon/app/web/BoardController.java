2a747bc2284d115fad836c43f0cfc2d5c3eacab9
yeolo03 <yeolo03@naver.com>
2020-05-07 11:15:53 +0900

    소스정

--- src/main/java/cf/garageon/app/web/BoardController.java
+++ src/main/java/cf/garageon/app/web/BoardController.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;

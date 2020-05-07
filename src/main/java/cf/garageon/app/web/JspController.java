6713933906b5bec1da26b6d184d63696b3fac8e0
yeolo03 <yeolo03@naver.com>
2020-05-08 07:07:29 +0900

    jspController test

---
+++ src/main/java/cf/garageon/app/web/JspController.java
@@ -0,0 +1,15 @@
+package cf.garageon.app.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JspController {
+
+    @RequestMapping("/")
+    public String jsp(Model model) throws Exception {
+        model.addAttribute("projectVer", "v1.2");
+        return "index";
+    }
+}

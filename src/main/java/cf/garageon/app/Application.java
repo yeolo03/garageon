560c6168bc91d8db7f065ec9851b03fcdea1fbd8
yeolo03 <yeolo03@naver.com>
2020-05-11 12:16:06 +0900

    nginx error

--- src/main/java/cf/garageon/app/Application.java
+++ src/main/java/cf/garageon/app/Application.java
@@ -3,7 +3,7 @@ package cf.garageon.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "cf.garageon.app.util")
+@SpringBootApplication(scanBasePackages = {"cf.garageon.app.util"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
